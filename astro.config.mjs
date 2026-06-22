cee155e41c7099ca3a2c7a02126cc66f45f74d09
vikirna <vikirna@proton.me>
2026-06-22 21:11:31 +0545

    react-icons needs react to work
    Signed-off-by: vikirna <vikirna@proton.me>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
